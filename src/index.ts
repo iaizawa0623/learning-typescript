f8ee658e93e3991e1cf25525fa00b297be755a55
ibuki aizawa <ibuki.aizawa@sota.co.jp>
2022-11-20 19:01:05 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log( 'Hello, TypeScript!')
+console.log('Hello, TypeScript!')
+
+interface p {
+  a?: number
+  b?: number
+}
+
+const f = ({ a, b }: p) => {
+  console.log(a)
+  console.log(b)
+}
+
+f({ a: 2, b: 3 })
+
+const f2 = (a: number, b: number) => {
+  console.log(a)
+  console.log(b)
+}
+f2(2, 3)
